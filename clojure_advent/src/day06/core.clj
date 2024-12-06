4838f2c0d37b096d3f94ea67534d4788266d6784
constantineMelios85 <melios@thinkdesquared.com>
2024-12-06 11:46:33 +0200

    part2

--- clojure_advent/src/day06/core.clj
+++ clojure_advent/src/day06/core.clj
@@ -20,7 +20,7 @@
                             :else acc)))
                       acc
                       (range cols)))
-            {:guard nil :obstacles [] :rows rows :cols cols}
+            {:guard nil :obstacles #{} :rows rows :cols cols}
             (range rows))))
 
 (defn change-direction []
@@ -31,30 +31,55 @@
 
 (defn in-grid? [pos rows cols]
   (let [[x y] pos]
-    (and (>= x 0) (< x cols) (>= y 0) (< y rows))))
+    (and (>= x 0) (< x rows) (>= y 0) (< y cols))))
 
 (defn patrol [obstacles rows cols]
-  (loop []
-    (let [[dx dy] (directions @current-direction)
-          [x y] @guard-position
-          new-pos [(+ x dx) (+ y dy)]]
-      (if (not (in-grid? new-pos rows cols))
-        ()
-        (if (some #{new-pos} obstacles)
+  (loop [visited-with-dir #{}]
+    (when-let [current-pos @guard-position]
+      (let [[dx dy] (directions @current-direction)
+            [x y] current-pos
+            new-pos [(+ x dx) (+ y dy)]]
+        (cond
+          (not (in-grid? new-pos rows cols))
+          {:result :out-of-grid :visited @visited-positions}
+          
+          (contains? obstacles new-pos)
           (do 
             (change-direction)
-            (recur))
+            (recur (conj visited-with-dir [current-pos @current-direction])))
+          
+          (contains? visited-with-dir [new-pos @current-direction])
+          {:result :loop :visited @visited-positions}
+          
+          :else
           (do
             (reset! guard-position new-pos)
             (swap! visited-positions conj new-pos)
-            (recur)))))))
+            (recur (conj visited-with-dir [new-pos @current-direction]))))))))
+
+(defn check-obstacle-causes-loop [grid pos]
+  (let [[row col] pos
+        modified-grid (update grid row #(str (subs % 0 col) "#" (subs % (inc col))))
+        coordinates (find-coordinates modified-grid)]
+    (reset! guard-position (:guard coordinates))
+    (reset! current-direction (first move-seq))
+    (reset! visited-positions #{@guard-position})
+    (= :loop (:result (patrol (:obstacles coordinates) (:rows coordinates) (:cols coordinates))))))
+
+(defn position-causes-loop? [coordinates grid pos]
+  (and (not (contains? (:obstacles coordinates) pos))
+       (check-obstacle-causes-loop grid pos)))
 
 (defn -main []
   (let [input (fr/read-lines "src/day06/input.txt")
         grid (mapv str/trim input)
-        coordinates (find-coordinates grid)]
+        coordinates (find-coordinates grid)] 
     (reset! guard-position (:guard coordinates))
     (reset! current-direction (first move-seq))
-    (reset! visited-positions #{@guard-position}) 
-    (patrol (:obstacles coordinates) (:rows coordinates) (:cols coordinates))
-    (println "Unique Spaces Counter:" (count @visited-positions))))
+    (reset! visited-positions #{@guard-position})
+    (let [initial-patrol (patrol (:obstacles coordinates) (:rows coordinates) (:cols coordinates))]
+      (println "Unique Spaces Counter:" (count (:visited initial-patrol))) 
+      (let [initial-positions (:visited initial-patrol)
+            loop-causing-positions (filter #(position-causes-loop? coordinates grid %) initial-positions)]
+        (println "Positions causing infinite loop:" (count loop-causing-positions))))))
+
